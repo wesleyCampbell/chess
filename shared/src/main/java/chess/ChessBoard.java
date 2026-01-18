c55249a66b757b5b9b9fbb45f023bb0949fdce8f
Wesley Campbell <wcamp64@byu.edu>
2026-01-17 20:13:41 -0700

    UPDATE: ChessBoard.java - Added hashCode() and equals(). Implemented support methods for above functions.
    Added hashCode() - Board now returns a hash based on the pieces on the board.
    Added equals() - ChessBoard objects are now equal if each of the pieces on the board are equal in position and type.
    Added setBoard() - Based on a string configuration, a ChessBoard will copy the pieces and place them on the board
    Added clearBoard() - Empties the board of all pieces
    Added resolveChessPiece - Will resolve a given ChessType.PieceType into a cooresponding character, or vice versa.
    	TODO: Will want to implement this with a hashmap, probably.
    Added validateBoardState() - Simple validation method for a string board config. Entirely based on geometry. Might be worthwhile to improve.

--- shared/src/main/java/chess/ChessBoard.java
+++ shared/src/main/java/chess/ChessBoard.java
@@ -1,5 +1,7 @@
 package chess;
 
+import java.util.InputMismatchException;
+
 /**
  * A chessboard that can hold and rearrange chess pieces.
  * <p>
@@ -7,16 +9,108 @@ package chess;
  * signature of the existing methods.
  */
 public class ChessBoard {
+	// ======================= STATIC ATTRIBUTES ======================
+
 	private static int DEFAULT_ROW_NUM = 8;
 	private static int DEFAULT_COL_NUM = 8;
 
-	private static ChessPiece[][] generateNewBoard(int rowNum, int colNum) {
+	private static String [] DEFAULT_BOARDSTATE = {
+		"RNBQKBNR",
+		"PPPPPPPP",
+		"--------",
+		"--------",
+		"--------",
+		"--------",
+		"PPPPPPPP",
+		"RNBQKBNR"
+	};
+
+	// ======================= STATIC METHODS ======================
+	/**
+	 * Generates a completely clean board
+	 *
+	 * @param rowNum: the number of rows in the board
+	 * @param colNum: the number of columns in the board
+	 */
+	private static ChessPiece[][] generateClearBoard(int rowNum, int colNum) {
 		return new ChessPiece[rowNum][colNum] ;	
 	}
+	/**
+	 * Verifies the dimentions of a game-state.
+	 *
+	 * @param boardState: an array of strings that represent the different rows of the board
+	 */
+	private static boolean validateBoardState(String[] boardState) {
+		int colNum = boardState[0].length();
+		for (int i = 1; i < boardState.length; i++) {
+			if (boardState[i].length() != colNum) {
+				return false;
+			}
+		}
 
+		return true;
+	}
+
+	/**
+	 * Returns a chess piece type based on a symbol.
+	 *
+	 * For example, 'R' will return ROOK.
+	 *
+	 * @param char pieceSymbol : The character representation of a piece
+	 *
+	 * @return The object type of the piece
+	 */
+	private static ChessPiece.PieceType resolveChessPiece(char pieceSymbol) {
+		switch(pieceSymbol) {
+			case 'R':
+				return ChessPiece.PieceType.ROOK;
+			case 'N':
+				return ChessPiece.PieceType.KNIGHT;
+			case 'B':
+				return ChessPiece.PieceType.BISHOP;
+			case 'Q':
+				return ChessPiece.PieceType.QUEEN;
+			case 'K':
+				return ChessPiece.PieceType.KING;
+			case 'P':
+				return ChessPiece.PieceType.PAWN;
+			default:
+				String err = String.format("Invalid piece symbol, %c", pieceSymbol);
+				throw new InputMismatchException(err);
+		}
+	}
+	
+	/**
+	 * Returns a character symbol based on a chess piece type
+	 *
+	 * @param pieceType : the chess piece type
+	 * 
+	 * @return The symbol representing a chess type
+	 */
+	private static char resolveChessPiece(ChessPiece.PieceType pieceType) {
+		switch(pieceType) {
+			case KING:
+				return 'K';
+			case QUEEN:
+				return 'Q';
+			case ROOK:
+				return 'R';
+			case KNIGHT:
+				return 'N';
+			case BISHOP:
+				return 'B';
+			case PAWN:
+				return 'P';
+			default:
+				return '?';
+		}
+	}
+
+	// ======================= MEMBER ATTRIBUTES ====================
 	int rowNum;
 	int colNum;
 
+	// ======================= CONSTRUTORS ==========================
 	private ChessPiece[][] board;
 
 	public ChessBoard() {
@@ -27,8 +121,10 @@ public class ChessBoard {
         // A 2D array representing the actual board
 		this.rowNum = rowNum;
 		this.colNum = colNum;
-		this.board = ChessBoard.generateNewBoard(rowNum, colNum); 
+		this.board = ChessBoard.generateClearBoard(rowNum, colNum); 
     }
+
+	// ==================== MEMBER METHODS =============================
 
     /**
      * Adds a chess piece to the chessboard
@@ -56,13 +152,57 @@ public class ChessBoard {
 
 		return this.board[row][col];
     }
+	
 
     /**
      * Sets the board to the default starting board
      * (How the game of chess normally starts)
      */
+	private void clearBoard() {
+		for (int i = 0; i < this.rowNum; i++) {
+			for (int j = 0; j < this.colNum; j++) {
+				this.board[i][j] = null;
+			}
+		}
+	}
+
+	private void setBoard(String[] newBoardState) {
+		
+		ChessBoard.validateBoardState(newBoardState);
+
+		for (int i = 0; i < this.rowNum; i++) {
+			String row = newBoardState[i];
+			ChessGame.TeamColor pieceColor;
+
+			// If we are on the top half of the board, the pieces are white.
+			// Black on the bottom half
+			if (i < rowNum / 2) {
+				pieceColor = ChessGame.TeamColor.WHITE;
+			} else {
+				pieceColor = ChessGame.TeamColor.BLACK;
+			}
+
+			for (int j = 0; j < colNum; j++) {
+				char pieceSymbol = row.charAt(j);
+				ChessPiece.PieceType pieceType;
+				
+				// Will try to resolve the piece representation to a valid piece.
+				// If it fails, the square will be empty.
+				try {
+					pieceType = ChessBoard.resolveChessPiece(pieceSymbol);
+				} catch (InputMismatchException e) {
+					board[i][j] = null;
+					continue;
+				}
+
+				board[i][j] = new ChessPiece(pieceColor, pieceType);
+			}
+		}
+
+	}
+
     public void resetBoard() {
-		this.board = generateNewBoard(this.rowNum, this.colNum);
+		this.setBoard(ChessBoard.DEFAULT_BOARDSTATE);
     }
 
 	/**
@@ -83,28 +223,7 @@ public class ChessBoard {
 					pieceSymbol = '-';
 					continue;
 				}
-				switch(piece.getPieceType()) {
-					case KING:
-						pieceSymbol = 'K';
-						break;
-					case QUEEN:
-						pieceSymbol = 'Q';
-						break;
-					case BISHOP:
-						pieceSymbol = 'B';
-						break;
-					case KNIGHT:
-						pieceSymbol = 'N';
-						break;
-					case ROOK:
-						pieceSymbol = 'R';
-						break;
-					case PAWN:
-						pieceSymbol = 'P';
-						break;
-					default:
-						pieceSymbol = '?';
-				}
+				pieceSymbol = ChessBoard.resolveChessPiece(piece.getPieceType());
 
 				outStr.append(String.format(" %c ", pieceSymbol));
 			}	
@@ -114,5 +233,56 @@ public class ChessBoard {
 			}
 		}
 		return outStr.toString(); 
+	}
+	
+	@Override
+	public int hashCode() {
+		int hash = 0;
+
+		for (int i = 0; i < this.rowNum; i++) {
+			for (int j = 0; j < this.colNum; j++) {
+				// If there is a piece on the spot, add the char value to the hash
+				// If not, just some index-based value
+				if (board[i][j] != null) {
+					ChessPiece.PieceType piece = this.board[i][j].getPieceType();
+					hash += (int) ChessBoard.resolveChessPiece(piece);
+				} else {
+					hash += i * 3 + j * 9;
+				}
+			}
+			hash *= 31;
+		}
+
+		return hash;
+	}
+
+	@Override
+	public boolean equals(Object obj) {
+		// Obj must exist and be of type ChessBoard
+		if (obj == null) { return false; }
+		if (obj.getClass() != ChessBoard.class) { return false; }
+
+		ChessBoard other = (ChessBoard) obj;
+
+		// Compare each spot. Each spot on the board must contain the same piece.
+		for (int i = 0; i < this.rowNum; i++) {
+			for (int j = 0; j < this.colNum; j++) {
+				ChessPosition pos = new ChessPosition(i + 1, j + 1);
+
+				ChessPiece thisPiece = this.getPiece(pos);
+				ChessPiece otherPiece = other.getPiece(pos);
+
+				// If one piece is null and the other is not
+				// or they are not equal to each other return false.
+				if (thisPiece == null) {
+					if (thisPiece != otherPiece) { return false; }
+				} else {
+					if (!thisPiece.equals(otherPiece)) { return false; }
+				}
+			}	
+		}
+		
+		// Nothing was different, must be equal.
+		return true;
 	}
 }
