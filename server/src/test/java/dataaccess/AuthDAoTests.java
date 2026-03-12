bf431954826b12ddfc64637b0395944768754356
Wesley Campbell <wcamp64@byu.edu>
2026-03-12 01:55:55 -0600

    TEST: Added Framework for DAO junit tests. Implemented AuthDAOTests.

---
+++ server/src/test/java/dataaccess/AuthDAoTests.java
@@ -0,0 +1,84 @@
+package dataaccess;
+
+import org.junit.jupiter.api.*;
+
+import dataaccess.sqldao.SQLAuthDAO;
+import model.AuthData;
+
+public class AuthDAoTests extends DatabaseDAoTests {
+	private static AuthDAO authDAO;
+	//
+	// ==================== TEST SETUP ===============
+	//
+	
+	@BeforeEach
+	public void init() {
+		authDAO = Assertions.assertDoesNotThrow(() -> new SQLAuthDAO());
+	}
+
+	//
+	// ==================== TEST CASES ================
+	//
+	
+	@Test
+	@Order(1)
+	public void authDataPersistance() {
+		AuthData data = Assertions.assertDoesNotThrow(() -> authDAO.getAuth(authToken1));
+
+		Assertions.assertEquals(data.username(), username1);
+	}
+
+	@Test
+	@Order(2)
+	public void authDataRemove() {
+		AuthData data = Assertions.assertDoesNotThrow(() -> authDAO.getAuth(authToken2));
+
+		Assertions.assertDoesNotThrow(() -> authDAO.removeAuth(data));
+
+		Assertions.assertThrows(AuthenticationException.class, () -> authDAO.getAuth(authToken2));
+	}
+
+	@Test
+	@Order(3)
+	public void authDataGetCorrect() {
+		AuthData data = Assertions.assertDoesNotThrow(() -> authDAO.getAuth(authToken1));
+
+		Assertions.assertEquals(data.authToken(), authToken1);
+		Assertions.assertEquals(data.username(), username1);
+	}
+
+	@Test
+	@Order(4)
+	public void authDataGetIncorrect() {
+		String badAuthToken = "heheheha";
+		Assertions.assertThrows(AuthenticationException.class, () -> authDAO.getAuth(badAuthToken));
+	}
+
+	@Test
+	@Order(5)
+	public void authDataInsert() {
+		String newAuthToken = "THIS IS A NEW AUTH TOKEN";
+		String newUsername = "newUsername";
+
+		Assertions.assertThrows(AuthenticationException.class, () -> authDAO.getAuth(newAuthToken));
+
+		AuthData newAuthData = new AuthData(newAuthToken, newUsername);
+		Assertions.assertDoesNotThrow(() -> authDAO.createAuth(newAuthData));
+
+		AuthData verifyData = Assertions.assertDoesNotThrow(() -> authDAO.getAuth(newAuthToken));
+
+		Assertions.assertEquals(newAuthData.authToken(), verifyData.authToken());
+		Assertions.assertEquals(newAuthData.username(), verifyData.username());
+	}
+
+	@Test
+	@Order(6)
+	public void authDataClear() {
+		Assertions.assertDoesNotThrow(() -> authDAO.getAuth(authToken1));
+
+		Assertions.assertDoesNotThrow(() -> authDAO.clearAllAuthData());
+
+		Assertions.assertThrows(AuthenticationException.class, () -> authDAO.getAuth(authToken1));
+	}
+
+}
---
+++ server/src/test/java/dataaccess/DatabaseDAoTests.java
@@ -0,0 +1,72 @@
+package dataaccess;
+
+import chess.*;
+import dataaccess.sqldao.*;
+import model.AuthData;
+import model.GameData;
+import model.UserData;
+
+import java.util.UUID;
+
+import org.junit.jupiter.api.*;
+
+@TestMethodOrder(MethodOrderer.OrderAnnotation.class)
+public abstract class DatabaseDAoTests {
+	protected static GameData gameData;
+
+	protected static String username1;
+	protected static String password1;
+	protected static String username2;
+	protected static String password2;
+	protected static String authToken1; 
+	protected static String authToken2; 
+
+	protected static String gameName;
+	protected static String gameID;
+
+	/**
+	 * Sets up some default user, game, and auth data to test on.
+	 * Also tests insertion for all DAOs.
+	 */
+	@BeforeAll
+	public static void initAll() {
+		AuthDAO authDAO;
+		GameDAO gameDAO;
+		UserDAO userDAO;
+
+		try {
+			authDAO = new SQLAuthDAO();
+			gameDAO = new SQLGameDAO();
+			userDAO = new SQLUserDAO();
+		} catch (DataAccessException ex) {
+			throw new RuntimeException("Couldn't connect to DAOS: " + ex.getMessage(), ex);
+		}
+
+		// Create users
+		username1 = "joe";
+		username2 = "samwise gamgee";
+		password1 = "joepassword";
+		password2 = "sampassword";
+
+		UserData userData1 = new UserData(username1, password1, "email1");
+		UserData userData2 = new UserData(username2, password2, "email2");
+		
+		Assertions.assertDoesNotThrow(() -> userDAO.createUser(userData1));
+		Assertions.assertDoesNotThrow(() -> userDAO.createUser(userData2));
+
+		// Create auth data
+		authToken1 = UUID.randomUUID().toString();
+		authToken2 = UUID.randomUUID().toString();
+
+		AuthData authData1 = new AuthData(authToken1, username1);
+		AuthData authData2 = new AuthData(authToken2, username2);
+
+		Assertions.assertDoesNotThrow(() -> authDAO.createAuth(authData1));
+		Assertions.assertDoesNotThrow(() -> authDAO.createAuth(authData2));
+
+		// Create game data
+		String gameName = "myFunGame";
+
+		gameData = Assertions.assertDoesNotThrow(() -> gameDAO.createGame(gameName));
+	}
+}
